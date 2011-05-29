6c3a0fe60b6f7095ba845552277fd15b5879e9ce
Wesley Hartford <github@cutterslade.ca>
2011-05-28 20:51:00 -0700

    Fixed up BaseTypesTest and added similar for BaseFields

---
+++ model/src/test/java/ca/cutterslade/edbafakac/model/BaseFieldsTest.java
@@ -0,0 +1,42 @@
+package ca.cutterslade.edbafakac.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.google.common.collect.ImmutableList;
+
+@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+@RunWith(Parameterized.class)
+public class BaseFieldsTest {
+
+  @Parameters
+  public static Collection<Object[]> getParameters() {
+    final ImmutableList.Builder<Object[]> builder = ImmutableList.builder();
+    for (final BaseField field : BaseField.values()) {
+      builder.add(new Object[]{ field });
+    }
+    return builder.build();
+  }
+
+  private final BaseField baseField;
+
+  public BaseFieldsTest(final BaseField baseField) {
+    this.baseField = baseField;
+  }
+
+  @Test
+  public void testField() {
+    final FieldValue field = baseField.getField();
+    assertNotNull(field);
+    assertNotNull(field.getName(true));
+    assertEquals(BaseTypesTest.getName(baseField), field.getName(true).getBaseValue());
+    assertNotNull(field.getFieldType(true));
+  }
+}
--- model/src/test/java/ca/cutterslade/edbafakac/model/BaseTypesTest.java
+++ model/src/test/java/ca/cutterslade/edbafakac/model/BaseTypesTest.java
@@ -4,13 +4,36 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.google.common.collect.ImmutableList;
+
+@SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+@RunWith(Parameterized.class)
 public class BaseTypesTest {
 
+  @Parameters
+  public static Collection<Object[]> getParameters() {
+    final ImmutableList.Builder<Object[]> builder = ImmutableList.builder();
+    for (final BaseType type : BaseType.values()) {
+      builder.add(new Object[]{ type });
+    }
+    return builder.build();
+  }
+
+  private final BaseType baseType;
+
+  public BaseTypesTest(final BaseType baseType) {
+    this.baseType = baseType;
+  }
+
   @Test
-  public void testBooleanType() {
-    final BaseType baseType = BaseType.BOOLEAN;
+  public void testType() {
     final TypeValue type = baseType.getType();
     assertNotNull(type);
     assertEquals(baseType.getKey(), type.getKey());
@@ -29,175 +52,7 @@ public class BaseTypesTest {
     }
   }
 
-  @Test
-  public void testDateType() {
-    final BaseType baseType = BaseType.DATE;
-    final TypeValue type = baseType.getType();
-    assertNotNull(type);
-    assertEquals(baseType.getKey(), type.getKey());
-    assertNotNull(type.getName(true));
-    assertEquals(getName(baseType), type.getName(true).getBaseValue());
-    final ListValue fields = type.getTypeFields(true);
-    assertNotNull(fields);
-    assertEquals(BaseType.FIELD.getType(), fields.getValueType(true));
-    assertTrue(0 < fields.getSize());
-    final long size = fields.getSize();
-    for (long i = 0; i < size; i++) {
-      final FieldValue field = (FieldValue) fields.get(i);
-      assertNotNull(field);
-      assertNotNull(field.getFieldType(true));
-      assertNotNull(field.getName(true));
-    }
-  }
-
-  @Test
-  public void testDecimalType() {
-    final BaseType baseType = BaseType.DECIMAL;
-    final TypeValue type = baseType.getType();
-    assertNotNull(type);
-    assertEquals(baseType.getKey(), type.getKey());
-    assertNotNull(type.getName(true));
-    assertEquals(getName(baseType), type.getName(true).getBaseValue());
-    final ListValue fields = type.getTypeFields(true);
-    assertNotNull(fields);
-    assertEquals(BaseType.FIELD.getType(), fields.getValueType(true));
-    assertTrue(0 < fields.getSize());
-    final long size = fields.getSize();
-    for (long i = 0; i < size; i++) {
-      final FieldValue field = (FieldValue) fields.get(i);
-      assertNotNull(field);
-      assertNotNull(field.getFieldType(true));
-      assertNotNull(field.getName(true));
-    }
-  }
-
-  @Test
-  public void testIntegerType() {
-    final BaseType baseType = BaseType.INTEGER;
-    final TypeValue type = baseType.getType();
-    assertNotNull(type);
-    assertEquals(baseType.getKey(), type.getKey());
-    assertNotNull(type.getName(true));
-    assertEquals(getName(baseType), type.getName(true).getBaseValue());
-    final ListValue fields = type.getTypeFields(true);
-    assertNotNull(fields);
-    assertEquals(BaseType.FIELD.getType(), fields.getValueType(true));
-    assertTrue(0 < fields.getSize());
-    final long size = fields.getSize();
-    for (long i = 0; i < size; i++) {
-      final FieldValue field = (FieldValue) fields.get(i);
-      assertNotNull(field);
-      assertNotNull(field.getFieldType(true));
-      assertNotNull(field.getName(true));
-    }
-  }
-
-  @Test
-  public void testListType() {
-    final BaseType baseType = BaseType.LIST;
-    final TypeValue type = baseType.getType();
-    assertNotNull(type);
-    assertEquals(baseType.getKey(), type.getKey());
-    assertNotNull(type.getName(true));
-    assertEquals(getName(baseType), type.getName(true).getBaseValue());
-    final ListValue fields = type.getTypeFields(true);
-    assertNotNull(fields);
-    assertEquals(BaseType.FIELD.getType(), fields.getValueType(true));
-    assertTrue(0 < fields.getSize());
-    final long size = fields.getSize();
-    for (long i = 0; i < size; i++) {
-      final FieldValue field = (FieldValue) fields.get(i);
-      assertNotNull(field);
-      assertNotNull(field.getFieldType(true));
-      assertNotNull(field.getName(true));
-    }
-  }
-
-  @Test
-  public void testRawType() {
-    final BaseType baseType = BaseType.RAW;
-    final TypeValue type = baseType.getType();
-    assertNotNull(type);
-    assertEquals(baseType.getKey(), type.getKey());
-    assertNotNull(type.getName(true));
-    assertEquals(getName(baseType), type.getName(true).getBaseValue());
-    final ListValue fields = type.getTypeFields(true);
-    assertNotNull(fields);
-    assertEquals(BaseType.FIELD.getType(), fields.getValueType(true));
-    assertTrue(0 < fields.getSize());
-    final long size = fields.getSize();
-    for (long i = 0; i < size; i++) {
-      final FieldValue field = (FieldValue) fields.get(i);
-      assertNotNull(field);
-      assertNotNull(field.getFieldType(true));
-      assertNotNull(field.getName(true));
-    }
-  }
-
-  @Test
-  public void testStringType() {
-    final BaseType baseType = BaseType.STRING;
-    final TypeValue type = baseType.getType();
-    assertNotNull(type);
-    assertEquals(baseType.getKey(), type.getKey());
-    assertNotNull(type.getName(true));
-    assertEquals(getName(baseType), type.getName(true).getBaseValue());
-    final ListValue fields = type.getTypeFields(true);
-    assertNotNull(fields);
-    assertEquals(BaseType.FIELD.getType(), fields.getValueType(true));
-    assertTrue(0 < fields.getSize());
-    final long size = fields.getSize();
-    for (long i = 0; i < size; i++) {
-      final FieldValue field = (FieldValue) fields.get(i);
-      assertNotNull(field);
-      assertNotNull(field.getFieldType(true));
-      assertNotNull(field.getName(true));
-    }
-  }
-
-  @Test
-  public void testFieldType() {
-    final BaseType baseType = BaseType.FIELD;
-    final TypeValue type = baseType.getType();
-    assertNotNull(type);
-    assertEquals(baseType.getKey(), type.getKey());
-    assertNotNull(type.getName(true));
-    assertEquals(getName(baseType), type.getName(true).getBaseValue());
-    final ListValue fields = type.getTypeFields(true);
-    assertNotNull(fields);
-    assertEquals(BaseType.FIELD.getType(), fields.getValueType(true));
-    assertTrue(0 < fields.getSize());
-    final long size = fields.getSize();
-    for (long i = 0; i < size; i++) {
-      final FieldValue field = (FieldValue) fields.get(i);
-      assertNotNull(field);
-      assertNotNull(field.getFieldType(true));
-      assertNotNull(field.getName(true));
-    }
-  }
-
-  @Test
-  public void testTypeType() {
-    final BaseType baseType = BaseType.TYPE;
-    final TypeValue type = baseType.getType();
-    assertNotNull(type);
-    assertEquals(baseType.getKey(), type.getKey());
-    assertNotNull(type.getName(true));
-    assertEquals(getName(baseType), type.getName(true).getBaseValue());
-    final ListValue fields = type.getTypeFields(true);
-    assertNotNull(fields);
-    assertEquals(BaseType.FIELD.getType(), fields.getValueType(true));
-    assertTrue(0 < fields.getSize());
-    final long size = fields.getSize();
-    for (long i = 0; i < size; i++) {
-      final FieldValue field = (FieldValue) fields.get(i);
-      assertNotNull(field);
-      assertNotNull(field.getFieldType(true));
-      assertNotNull(field.getName(true));
-    }
-  }
-
-  private String getName(final BaseType type) {
-    return BaseType.class.getSimpleName() + '.' + type.toString();
+  public static String getName(final Enum<?> type) {
+    return type.getDeclaringClass().getSimpleName() + '.' + type.toString();
   }
 }
