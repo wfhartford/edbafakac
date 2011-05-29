c6f10fca88b8095aada1d04e381c746d569be3c7
Wesley Hartford <github@cutterslade.ca>
2011-05-29 12:02:20 -0700

    Improved StringValue test

--- model/src/test/java/ca/cutterslade/edbafakac/model/StringValueTest.java
+++ model/src/test/java/ca/cutterslade/edbafakac/model/StringValueTest.java
@@ -10,11 +10,28 @@ public class StringValueTest {
 
   private static final String HELLO_WORLD = "Hello, World!";
 
-  private static final String HELLO_WORLD_FR = "Bonjour tout le monde!";
+  private static final String HELLO_WORLD_FR_CA = "Bonjour tout le monde!";
+
+  private static final String HELLO_WORLD_FR_FR = "Bonjour tout le monde";
 
   @Test
   public void twoLanguangeTest() {
-    final StringValue value = StringValue.withValue(HELLO_WORLD, Locale.CANADA);
+    StringValue value = StringValue.withValue(HELLO_WORLD, Locale.CANADA);
+    assertEquals(value, value.getName(true));
+    testSingleLocale(value);
+    value.setValue(HELLO_WORLD_FR_CA, Locale.CANADA_FRENCH);
+    testDoubleLocale(value);
+    value = (StringValue) Values.getValue(value.save().getKey(), false);
+    assertEquals(value, value.getName(true));
+    testDoubleLocale(value);
+    value.setValue(HELLO_WORLD_FR_FR, Locale.FRANCE);
+    testTripleLocale(value);
+    value = (StringValue) Values.getValue(value.save().getKey(), false);
+    assertEquals(value, value.getName(true));
+    testTripleLocale(value);
+  }
+
+  private void testSingleLocale(final StringValue value) {
     assertEquals(HELLO_WORLD, value.getValue(Locale.CANADA));
     assertEquals(HELLO_WORLD, value.getValue(Locale.ENGLISH));
     assertEquals(HELLO_WORLD, value.getBaseValue());
@@ -24,15 +41,29 @@ public class StringValueTest {
     assertEquals(HELLO_WORLD, value.getValue(Locale.FRANCE));
     assertEquals(HELLO_WORLD, value.getValue(Locale.FRENCH));
     assertEquals(HELLO_WORLD, value.getValue(Locale.ROOT));
-    value.setValue(HELLO_WORLD_FR, Locale.CANADA_FRENCH);
+  }
+
+  private void testDoubleLocale(final StringValue value) {
     assertEquals(HELLO_WORLD, value.getValue(Locale.CANADA));
     assertEquals(HELLO_WORLD, value.getValue(Locale.ENGLISH));
     assertEquals(HELLO_WORLD, value.getBaseValue());
     assertEquals(HELLO_WORLD, value.getValue(Locale.US));
     assertEquals(HELLO_WORLD, value.getValue(Locale.TRADITIONAL_CHINESE));
-    assertEquals(HELLO_WORLD_FR, value.getValue(Locale.CANADA_FRENCH));
-    assertEquals(HELLO_WORLD_FR, value.getValue(Locale.FRANCE));
-    assertEquals(HELLO_WORLD_FR, value.getValue(Locale.FRENCH));
+    assertEquals(HELLO_WORLD_FR_CA, value.getValue(Locale.CANADA_FRENCH));
+    assertEquals(HELLO_WORLD_FR_CA, value.getValue(Locale.FRANCE));
+    assertEquals(HELLO_WORLD_FR_CA, value.getValue(Locale.FRENCH));
+    assertEquals(HELLO_WORLD, value.getValue(Locale.ROOT));
+  }
+
+  private void testTripleLocale(final StringValue value) {
+    assertEquals(HELLO_WORLD, value.getValue(Locale.CANADA));
+    assertEquals(HELLO_WORLD, value.getValue(Locale.ENGLISH));
+    assertEquals(HELLO_WORLD, value.getBaseValue());
+    assertEquals(HELLO_WORLD, value.getValue(Locale.US));
+    assertEquals(HELLO_WORLD, value.getValue(Locale.TRADITIONAL_CHINESE));
+    assertEquals(HELLO_WORLD_FR_CA, value.getValue(Locale.CANADA_FRENCH));
+    assertEquals(HELLO_WORLD_FR_FR, value.getValue(Locale.FRANCE));
+    assertEquals(HELLO_WORLD_FR_CA, value.getValue(Locale.FRENCH));
     assertEquals(HELLO_WORLD, value.getValue(Locale.ROOT));
   }
 }
